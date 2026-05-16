ba39cd73144132649df9d04cc392453dc3054c24
kuba-bednarczyk <kubabednarczyk2003@gmail.com>
2026-05-16 16:10:08 +0200

    chore: setup vite + TailwindCSS v4 + shadcn

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+
+  return (
+    <>
+     <div className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-zinc-50">
+      <h1 className="text-3xl font-bold text-zinc-900">Frontend odpalony!</h1>
+      <Button>Mój pierwszy przycisk z shadcn</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
