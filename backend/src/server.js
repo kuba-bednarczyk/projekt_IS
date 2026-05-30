ea5bb217fd3b1409ed4906a449992d4a35a95c3a
jedrzej-bryk <284678433+jedrzej-bryk@users.noreply.github.com>
2026-05-30 23:20:21 +0200

    feat: konteneryzacja na podstawie Dockerfile

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,12 +15,13 @@ const app = express();
 
 // Middleware
 // zmiana ustawien cors pod httpCookie
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+//DEPRECATED CORS
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }),
+// ); 
 
 app.use(express.json({ limit: "5mb" })); // pozwala czytac dane z bazy (limit zwiększony dla zdjęć Base64)
 app.use(cookieParser()); // do obsługi ciasteczek HttpOnly
@@ -47,6 +48,6 @@ app.get("/api/status", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on : http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on : http://localhost:${PORT}`);
+// });
